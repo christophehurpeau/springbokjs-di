2c7f54efb3f35c5b9821e51c45493f1b292cde45
Christophe Hurpeau <christophe@hurpeau.com>
2014-10-20 00:16:13 +0200

    0.1.0

--- tests/lib/test.js
+++ tests/lib/test.js
@@ -1,22 +1,29 @@
+/* global test */
 "use strict";
 require('es6-shim');
 var assert = require('proclaim');
 var expect = assert.strictEqual;
 var lib = '../../lib' + (process.env.TEST_COV && '-cov' || '') + '/';
+
 var Di = require(lib).Di;
+
 test('should load classes in the directory and return the right result', function() {
-  var di = new Di();
-  return di.directory(__dirname + '/../../samples/').then(function() {
-    assert.isFunction(di._classes.ClassA);
-    assert.isFunction(di._classes.ClassB);
-    assert.isFunction(di._classes.ClassC);
-    assert.isFunction(di._classes.ClassD);
-    return Promise.all([di.getInitializedInstance('ClassC').then(function(classC) {
-      expect('Hello John!', classC.sayHello());
-    }), di.getInitializedInstance('ClassD').then(function(classD) {
-      expect('Hello John!', classD.sayHello());
-    })]);
-  });
+    var di = new Di();
+    return di.directory(__dirname + '/../../samples/').then(function() {
+        assert.isFunction(di._classes.ClassA);
+        assert.isFunction(di._classes.ClassB);
+        assert.isFunction(di._classes.ClassC);
+        assert.isFunction(di._classes.ClassD);
+
+        return Promise.all([
+            di.getInitializedInstance('ClassC').then(function(classC) {
+                expect('Hello John!', classC.sayHello());
+            }),
+            di.getInitializedInstance('ClassD').then(function(classD) {
+                expect('Hello John!', classD.sayHello());
+            })
+        ]);
+    });
 });
 
 //# sourceMappingURL=test.js.map
